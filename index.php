d6136db9e355ba69b3e0176424b06074bbe00478
Marta Sousa <marta.sousa@gmail.com>
2019-09-29 15:32:21 -0300

    código reorganizado

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
-<!-- <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="#">Cursos<span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Login</a>
-            <a class="nav-item nav-link" href="#">Cadastrar</a>
-        </div>
-    </div>
-    </nav>   
-</header> -->
-
 <header class="d-flex justify-content-between align-items-center p-3">
     <h1 id="logo"><?php echo $nomeSistema;?></h1>
     <nav>
@@ -43,37 +27,38 @@
 
 
 
-    // <?php
-    //     $nome = "Marta";
-    //     $idade = 30;
+    <?php
+/*      $nome = "Marta";
+        $idade = 30;
 
-    //     //array numérico
-    //     $usuario = ["Marta","Sousa",30, ["trilha","pintura","netflix"]];
+        //array numérico
+        $usuario = ["Marta","Sousa",30, ["trilha","pintura","netflix"]];
 
-    //     //array associativo
-    //     $usuarioDois = ["nome"=>"marta","sobrenome"=>"Sousa","idade"=>30];
+        //array associativo
+        $usuarioDois = ["nome"=>"marta","sobrenome"=>"Sousa","idade"=>30];
 
-    //     echo $usuario[1];
-    //     echo $usuarioDois["sobrenome"];
+        echo $usuario[1];
+        echo $usuarioDois["sobrenome"];
 
-    //     echo "<br>";
+        echo "<br>";
 
-    //     var_dump($usuario);
+        var_dump($usuario);
 
-    //     echo $usuario[3][0];
+        echo $usuario[3][0];
 
-    //     echo "<br>";
-    //     //adicionando informações (fora da array)
-    //     $usuario[]="guitarra";
-    //     var_dump($usuario);
+        echo "<br>";
+        //adicionando informações (fora da array)
+        $usuario[]="guitarra";
+        var_dump($usuario);
         
-    //     //adicionando informações (dentro da array)
-    //     $usuario[3][]="passear";
-    //     echo "<br>";
-    //     var_dump($usuario);
+        //adicionando informações (dentro da array)
+        $usuario[3][]="passear";
+        echo "<br>";
+        var_dump($usuario);
 
-    //     echo "<br>";
-    //     echo "<h1>".$usuario[0]." ".$usuario[1]."<br>"."</h1>";
-    // ?>
+        echo "<br>";
+        echo "<h1>".$usuario[0]." ".$usuario[1]."<br>"."</h1>"; */
+    ?>
+    
 </body>
 </html>
